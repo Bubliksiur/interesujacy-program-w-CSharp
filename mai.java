6d7fe031e243c0db51a43332f2166479d90c2732
Bubliksiur <149240768+Bubliksiur@users.noreply.github.com>
2025-01-29 21:09:42 +0100

    Java loops

---
+++ mai.java
@@ -0,0 +1,12 @@
+
+public class mai {
+
+    public static void main(String[] args) {
+        System.out.println("I love spongebob");
+
+        for (int i = 1; i = 20; i++) {
+            System.out.println("I love spongebob");
+        }
+    }
+
+}
